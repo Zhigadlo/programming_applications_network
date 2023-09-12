9dfa3646e62a1cf7f5ac9d26fe9313d329210652
Vladislav Zhogol <vzhogol176@gmail.com>
2023-09-12 18:29:53 +0300

    Adding mathematical expression sending

--- lab1/TcpClient/Program.cs
+++ lab1/TcpClient/Program.cs
@@ -6,19 +6,48 @@ using var tcpClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, 
 try
 {
     await tcpClient.ConnectAsync(new IPAddress(new byte[] { 127, 0, 0, 1 }), 8888);
+    while (true)
+    {
+        string expression = GetExpression();
 
-    byte[] data = Encoding.UTF8.GetBytes("Сосново шишечко, навишчо ты пьешь?");
-    // отправляем данные
-    await tcpClient.SendAsync(data);
-    // буфер для считывания одного байта
-    var bytesRead = new byte[512];
-    
-    tcpClient.Receive(bytesRead);
+        byte[] data = Encoding.UTF8.GetBytes(expression);
+        // отправляем данные
+        await tcpClient.SendAsync(data);
+        Thread.Sleep(1000);
+        double x = GetDouble();
 
-    Console.WriteLine(Encoding.UTF8.GetString(bytesRead));
+        await tcpClient.SendAsync(Encoding.UTF8.GetBytes(x.ToString()));
+        Console.WriteLine("Message was sent. Waiting for answer...");
+        // буфер для считывания одного байта
+        var bytesRead = new byte[512];
+
+        tcpClient.Receive(bytesRead);
+
+        Console.WriteLine(Encoding.UTF8.GetString(bytesRead));
+        Console.ReadKey();
+    }
 }
 catch (Exception ex)
 {
     Console.WriteLine(ex.Message);
 }
-Console.ReadKey();
+
+string GetExpression()
+{
+    //string expression = String.Empty;
+    //while (true)
+    //{
+    //    Console.Write("Enter mathematical expression: ");
+    //    expression = Console.ReadLine();
+
+    //    if (expression != String.Empty)
+    //        break;
+    //}
+    //return expression;
+    return "x^2";
+}
+
+double GetDouble()
+{
+    return 9.1;
+}
