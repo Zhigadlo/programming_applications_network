214d8d68b6957a77cf81b87428cc48c096f0c905
Vladislav Zhogol <vzhogol176@gmail.com>
2023-09-13 17:04:32 +0300

    Server logic completion

--- lab1/TcpServer/Program.cs
+++ lab1/TcpServer/Program.cs
@@ -1,22 +1,23 @@
 ﻿using System.Net;
+using System.Net.Http;
 using System.Net.Sockets;
 using System.Text;
+using TcpServer;
 
 using Socket tcpListener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
 try
 {
     tcpListener.Bind(new IPEndPoint(new IPAddress(new byte[] { 127, 0, 0, 1 }), 8888));
-    tcpListener.Listen();    // запускаем сервер
-    Console.WriteLine("Сервер запущен. Ожидание подключений... ");
+    tcpListener.Listen();
+    Console.WriteLine("Server is running. Waiting for connection... ");
 
     while (true)
     {
-        // получаем подключение в виде TcpClient
         var tcpClient = await tcpListener.AcceptAsync();
 
-        // создаем новую задачу для обслуживания нового клиента
         Task.Run(async () => await ProcessClientAsync(tcpClient));
+        
     }
 }
 catch (Exception ex)
@@ -26,17 +27,35 @@ catch (Exception ex)
 
 async Task ProcessClientAsync(Socket tcpClient)
 {
-    var bytesRead = new byte[512];
+    Console.WriteLine("Server is waiting for client messages");
+
+    var bytesRead = new byte[64];
 
     await tcpClient.ReceiveAsync(bytesRead);
-    Mathos.Parser.MathParser parser = new Mathos.Parser.MathParser();
     var expression = Encoding.UTF8.GetString(bytesRead);
-    Console.WriteLine(expression);
+    Console.WriteLine($"expression: {expression}");
+    bytesRead = new byte[64];
+
     await tcpClient.ReceiveAsync(bytesRead);
-    double x = Convert.ToDouble(Encoding.UTF8.GetString(bytesRead));
-    Console.WriteLine(x);
-    parser.LocalVariables.Add("x", x);
-    double result = parser.Parse(expression);
+    double xStart = double.Parse(Encoding.UTF8.GetString(bytesRead));
+    Console.WriteLine($"xStart: {xStart}");
+    bytesRead = new byte[64];
+
+    await tcpClient.ReceiveAsync(bytesRead);
+    double learningRate = double.Parse(Encoding.UTF8.GetString(bytesRead));
+    Console.WriteLine($"learningRate: {learningRate}");
+    bytesRead = new byte[64];
+
+    await tcpClient.ReceiveAsync(bytesRead);
+    int numIteration = int.Parse(Encoding.UTF8.GetString(bytesRead));
+    Console.WriteLine($"numOfIteration: {numIteration}");
+
+    GradientDescentSolver solver = new GradientDescentSolver(expression);
+    solver.SetxStart(xStart);
+    solver.SetLearningRate(learningRate);
+    solver.SetNumberOfIteration(numIteration);
+
+    double result = solver.Solve();
 
     await tcpClient.SendAsync(Encoding.UTF8.GetBytes(result.ToString()));
 }
